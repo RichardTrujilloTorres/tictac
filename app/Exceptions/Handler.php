b428672d6651af3b78e644280c43654307307a9a
Richard Trujillo <richard@desemax.com>
2018-09-18 21:18:13 +0200

    Set ModelNotFoundException handler response

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -45,6 +45,13 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof ModelNotFoundException) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $exception->getMessage(),
+            ], 404);
+        }
+
         return parent::render($request, $exception);
     }
 }
